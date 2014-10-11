21e1dff642c3bb6ff0e74bbc03f53b36d43c8798
hussainm <hussainm+git@marketlytics.com>
2014-10-11 17:00:39 +0500

    Misc fixes
    fixes to inject.js

--- ext/src/inject/inject.js
+++ ext/src/inject/inject.js
@@ -5,57 +5,68 @@ chrome.extension.sendMessage({}, function(response) {
 
 		// ----------------------------------------------------------
 		// This part of the script triggers when page is done loading
-		console.log("Hello. This message was sent from scripts/inject.js");
+		// console.log("Hello. This message was sent from scripts/inject.js");
 		// ----------------------------------------------------------
 
 		// load jquery
 		// var jq = document.createElement('script');
 		// jq.src = "//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js";
 		// document.getElementsByTagName('head')[0].appendChild(jq);
-		var callingBack = function(callBack) {
-			var selectedOption = ' ';
+
+		var selectedOption;
+		var selectedSeparator;
+
+		var callingBack = function(callBack) {			
 			chrome.storage.sync.get(function (obj) {
 				selectedOption = obj.option;
 				selectedSeparator = obj.separator;
+				if(selectedOption == undefined) {
+					selectedOption = 'type';
+				}
+				if(selectedSeparator == undefined) {
+					selectedSeparator = '-';
+				}
 				callBack(selectedOption, selectedSeparator);
 			});
 		}
 
 			var gtmTableUpdate = function(selectedOption, selectedSeparator){
-				if(selectedOption == undefined) {
-					selectedOption = 'name';
-				}
-				if(selectedSeparator == undefined) {
-					selectedSeparator = '-';
-				}
+
+			if(selectedOption == "type"){
+				jQuery('div#ID-tagTable div.ID-table tbody tr th.TARGET-1').click();
+			}				
+			// else if (selectedOption == "name"){
+			// 	jQuery('div#ID-tagTable div.ID-table tbody tr th.TARGET-0').click();
+			// }
+
 			var prevAttr = "";
 
 			// parse and append tag
 			jQuery('div#ID-tagTable div.ID-table tbody tr.CT_TABLE_ROW').each(function(){
-				var tag = jQuery(this).find('td.CT_TABLE_CELL>div.ACTION-clickTag').text().split(selectedSeparator);
-				var type = jQuery(this).find('td.CT_TABLE_CELL:nth-child(2)').text();
-				// tag[0] = (tag.length > 1) ? tag[0] : "Other";
-
-				//append tag to parent
-				if(tag.length > 1){
-					jQuery(this).attr('tagGroup', tag[0]);
-				}
-
-				jQuery(this).attr('tagtype', type);
-
+			
 				console.log(selectedOption);
 				// var selectedOption = "type";
 				if(selectedOption == "type"){
-					var curAttr = jQuery(this).attr('tagtype');
+
+					var type = jQuery(this).find('td.CT_TABLE_CELL:nth-child(2)').text();
+					// tag[0] = (tag.length > 1) ? tag[0] : "Other";
+					jQuery(this).attr('tagType', type);
+
+					var curAttr = jQuery(this).attr('tagType');
 					if(curAttr != undefined && curAttr != prevAttr){
-						jQuery(this).before('<tr class="toggle" id="'+curAttr+'"><td>'+curAttr+'</td><td></td><td></td><td></td></tr>');
+						jQuery(this).before('<tr class="toggle" id="'+curAttr+'" toggletype="tagType"><td>'+curAttr+'</td><td></td><td></td><td></td></tr>');
 						prevAttr = curAttr;
 					}
 
-				} else {
-					var curAttr = jQuery(this).attr('tagGroup');
+				} else if (selectedOption == "name") {
+					var tag = jQuery(this).find('td.CT_TABLE_CELL>div.ACTION-clickTag').text().split(selectedSeparator);
+					//append tag to parent
+					if(tag.length > 1){
+						jQuery(this).attr('tagName', tag[0]);
+					}
+					var curAttr = jQuery(this).attr('tagName');
 					if(curAttr != undefined && curAttr != prevAttr){
-						jQuery(this).before('<tr class="toggle" id="'+curAttr+'"><td>'+curAttr+'</td><td></td><td></td><td></td></tr>');
+						jQuery(this).before('<tr class="toggle" id="'+curAttr+'" toggletype="tagName"><td>'+curAttr+'</td><td></td><td></td><td></td></tr>');
 						prevAttr = curAttr;
 					}
 				}
@@ -63,26 +74,52 @@ chrome.extension.sendMessage({}, function(response) {
 
 
 			// Toggle Rows
+			var toggleType;
 			jQuery('.toggle').click(function(){
 			var id = jQuery(this).attr('id');
-			jQuery('tr[tagGroup="'+id+'"]').toggle();
-			});
+			toggleType = jQuery(this).attr('toggletype');
+						
+				jQuery('tr['+toggleType+'="'+id+'"]').toggle();	
+			});			
+			//jQuery('tr['toggleType']').toggle();
+			jQuery('tr[tagname]').hide();
+			jQuery('tr[tagtype]').hide();
 
-			jQuery('tr[taggroup]').toggle();
+
 		}
 
 		var tableRedraw = function(){
 		jQuery('div.ID-table table.CT_TABLE tbody th').click(function(){
 			setTimeout(function(){
-			gtmTableUpdate(); tableRedraw();
-			// console.log("code run");
+				// re input data on table update 
+				gtmTableUpdate(selectedOption, selectedSeparator); 
+				tableRedraw();
+				// console.log("code run");
 			}, 500);
 		});
 		}
 
-		tableRedraw();
-		// gtmTableUpdate();
-		callingBack(gtmTableUpdate);
+	// 
+	tableRedraw();
+	callingBack(gtmTableUpdate);
+
+	// on hash change wait for page to load before applying changes
+	var waitAndLoad = function(t){
+    setTimeout(function(){
+     callingBack(gtmTableUpdate);
+    }, t)
+  }
+
+  var forceLoad = function(){
+    waitAndLoad(500);
+  }
+  // if hash changes #Container
+  window.onhashchange = function() {
+    forceLoad();
+  }
+  // document.onclick = function() {
+  //   forceLoad();
+  // }
 
 	}
 	}, 10);
